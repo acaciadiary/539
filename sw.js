47049912e1548210796f6f0bb5d9313ad8facd2f
dodoisgood <dodo567.huang@gmail.com>
2026-06-23 20:30:58 +0800

    up

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "five39-model-compare-v2";
+const CACHE_NAME = "five39-model-compare-v3";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./data/draws.json", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
