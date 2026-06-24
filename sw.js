448ffbb139a6194e090dc0e1ab3fabc64cd1c3d9
dodoisgood <dodo567.huang@gmail.com>
2026-06-24 14:28:20 +0800

    fix

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const CACHE_NAME = "five39-model-compare-v4";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./data/draws.json", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "five39-model-compare-v5";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./data/draws.json",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
